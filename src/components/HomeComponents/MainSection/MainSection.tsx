3754979de29f6420792028cff83019d2e31c71c1
Aleksandr Zotov <szotov223@gmail.com>
2025-12-06 00:39:36 +0500

    1.5.5.3 Fix

--- src/components/HomeComponents/MainSection/MainSection.tsx
+++ src/components/HomeComponents/MainSection/MainSection.tsx
@@ -5,7 +5,7 @@ const MainSection = () => {
 	return (
 		<div className='flex main-section max-lg:flex-col-reverse'>
 			<Text />
-			<div className='flex-1 rounded-xl overflow-hidden z-0 max-sm:pointer-events-none'>
+			<div className='flex-1 rounded-xl overflow-hidden z-0 max-sm:pointer-events-none max-sm:select-none'>
 				<Scene />
 			</div>
 		</div>
